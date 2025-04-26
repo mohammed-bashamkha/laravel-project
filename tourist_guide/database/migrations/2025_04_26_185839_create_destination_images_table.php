0fb2e55114216a81aa9e68e9a9bb549441973fe0
Mohammed_Bashamkha <mb877402@gmail.com>
2025-04-26 22:09:45 +0300

    added destination_id and image_path columns to destination_images table

--- tourist_guide/database/migrations/2025_04_26_185839_create_destination_images_table.php
+++ tourist_guide/database/migrations/2025_04_26_185839_create_destination_images_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('destination_images', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('destination_id')->constrained()->cascadeOnDelete();
+            $table->string('image_path');
             $table->timestamps();
         });
     }
