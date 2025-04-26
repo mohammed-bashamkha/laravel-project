e7e25caf0c2215809f479b122a1d7f9a0af0badb
Mohammed_Bashamkha <mb877402@gmail.com>
2025-04-26 22:28:47 +0300

    connecting the destination with images and agencies

--- tourist_guide/app/Models/Destination.php
+++ tourist_guide/app/Models/Destination.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
-    //
+    protected $fillable = ['name','description'];
+
+    public function images() {
+        return $this->hasMany(DestinationImage::class);
+    }
+
+    public function agencies() {
+        return $this->belongsToMany(Agency::class,'destination_agency');
+    }
 }
