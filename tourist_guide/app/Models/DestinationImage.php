5d40e8b149582987502b59226c7911f1676fe7e3
Mohammed_Bashamkha <mb877402@gmail.com>
2025-04-26 22:33:41 +0300

    connecting The Destination Image model with Destination model

--- tourist_guide/app/Models/DestinationImage.php
+++ tourist_guide/app/Models/DestinationImage.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DestinationImage extends Model
 {
-    //
+    protected $fillable = ['destination_id','image_path'];
+
+    public function destination() {
+        return $this->belongsTo(Destination::class);
+    }
 }
