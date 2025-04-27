5bd2f6d63ac7dd135724e73eb0ae6f384cc10669
Mohammed_Bashamkha <mb877402@gmail.com>
2025-04-27 09:04:53 +0300

    added app\Http\Requests\StoreDestinationRequest.php

--- tourist_guide/app/Http/Controllers/DestinationController.php
+++ tourist_guide/app/Http/Controllers/DestinationController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreDestinationRequest;
+use App\Models\Destination;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    //
+    public function store(StoreDestinationRequest $request) {
+        $validated = $request->validated();
+        $destination = Destination::create($validated);
+        return response()->json('Destination Added Seccssfuly', 200);
+    }
 }
--- tourist_guide/routes/api.php
+++ tourist_guide/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\DestinationController;
+use App\Models\Destination;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/destination',[DestinationController::class,'store']);
