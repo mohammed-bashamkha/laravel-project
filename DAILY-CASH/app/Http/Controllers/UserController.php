a051b7e0ad947dbb73a17053f73b63e1fedd504c
mohammed_bashamkha <mb877402@gmail.com>
2025-10-09 16:50:11 +0300

    add register and register method

--- DAILY-CASH/app/Http/Controllers/UserController.php
+++ DAILY-CASH/app/Http/Controllers/UserController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|string|max:255|unique:users,email',
+            'password' => 'required|string|password|min:8'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json([
+            'massage' => 'User Registered Successfuly',
+            'User' => $user,
+        ],201);
+    }
 }
