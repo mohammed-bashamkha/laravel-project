c593e4b9a0f71fad96385174a8eb36652dd2b865
Mohammed Bashamkha <mb877402@gmail.com>
2025-10-17 21:13:18 +0300

    feat: Create RevenuesExpenses model, controller, and migration for revenues_expenses table

---
+++ DAILY-CASH/database/migrations/2025_10_17_181210_create_revenues_expenses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revenues_expenses', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['income', 'expense']); // نوع العملية
+            $table->date('date');
+            $table->decimal('amount', 12, 2);
+            $table->text('description')->nullable();
+
+            // مفاتيح خارجية
+            $table->foreignId('entity_id')->nullable()->constrained('entities')->onDelete('set null');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revenues_expenses');
+    }
+};
